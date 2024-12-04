84224bfbbad2d6198e717aebbe2815e054e312b0
felicianpopa <felician.popa@bootintech.com>
2024-12-04 15:51:00 +0200

    Tailwind

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-    <header>
-        <nav>
-            <ul>
+    <header class="shadow-sm bg-white">
+        <nav class="container mx-auto p-4 flex justify-between">
+            <ul class="flex gap-4 items-center">
                 <li>
                     <NuxtLink to="/">Home</NuxtLink>
                 </li>
@@ -9,12 +9,14 @@
                     <NuxtLink to="/about">About</NuxtLink>
                 </li>
                 <li>
-                    <NuxtLink to="/products">Products</NuxtLink>
+                    <NuxtLink to="/products" class="btn">Products</NuxtLink>
                 </li>
             </ul>
         </nav>
     </header>
-    <slot />
+    <div class="container mx-auto p-4">
+        <slot />
+    </div>
 </template>
 
 <script>
