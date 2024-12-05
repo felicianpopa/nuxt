8e1c31e1ff8c9426b62785147dc02d362b775fe8
felicianpopa <felician.popa@bootintech.com>
2024-12-05 06:50:38 +0200

    Errors

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -9,7 +9,11 @@ definePageMeta({
 const { id } = useRoute().params;
 const uri = `https://fakestoreapi.com/products/${id}`;
 
-const { data: product } = await useFetch(uri)
+const { data: product } = await useFetch(uri);
+
+if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: `Product ${id} not found`, fatal: true })
+}
 </script>
 
 <style></style>
